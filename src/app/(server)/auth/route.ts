84764c2c1196ce634e53fc6f72146c2e919bb181
snehasishcodes <snehasishcodes@gmail.com>
2025-06-17 02:28:11 +0530

    fix: scope to user_scope for slack auth

--- src/app/(server)/auth/route.ts
+++ src/app/(server)/auth/route.ts
@@ -1,7 +1,7 @@
 export async function GET() {
     const params = new URLSearchParams({
         client_id: process.env.SLACK_CLIENT_ID!,
-        scope: "identity.basic,identity.email,identity.avatar",
+        user_scope: "identity.basic,identity.email,identity.avatar",
         redirect_uri: process.env.SLACK_REDIRECT_URI!,
     });
 
